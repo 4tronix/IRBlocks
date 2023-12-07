e4a60c22c4a3aa88e4b1e82b0824d22bc226614e
4tronix <Sales@4tronix.co.uk>
2023-12-07 19:45:52 +0000

    add back noAny

--- irblocks.ts
+++ irblocks.ts
@@ -2,7 +2,7 @@
 /**
   * Key code translations
   */
-enum irKeys
+enum IRBirKeys
 {
     //% block="any"
     Any=0,
@@ -43,6 +43,48 @@ enum irKeys
 }
 
 /**
+  * IR Key code translations without the Any code
+  */
+enum IRBirNoAny
+{
+    //% block="0"
+    Zero=152,
+    //% block="1"
+    One=162,
+    //% block="2"
+    Two=98,
+    //% block="3"
+    Three=226,
+    //% block="4"
+    Four=34,
+    //% block="5"
+    Five=2,
+    //% block="6"
+    Six=194,
+    //% block="7"
+    Seven=224,
+    //% block="8"
+    Eight=168,
+    //% block="9"
+    Nine=144,
+    //% block="star"
+    Star=104,
+    //% block="hash"
+    Hash=176,
+    //% block="up"
+    Up=24,
+    //% block="down"
+    Down=74,
+    //% block="left"
+    Left=16,
+    //% block="right"
+    Right=90,
+    //% block="ok"
+    Ok=56
+}
+
+
+/**
  * Makecode support for 4tronix IR Receiver
  */
 //% weight=50 color=#e7660b icon="\uf0a3"
@@ -58,7 +100,7 @@ namespace irBlocks
     //% weight=100
     //% blockId=onIrEvent
     //% block="on 14 IR pin%pin|key%key"
-    export function onIREvent(pin: DigitalPin, event: irKeys, handler: Action)
+    export function onIREvent(pin: DigitalPin, event: IRBirKeys, handler: Action)
     {
         irCore.initEvents(pin)
         control.onEvent(irEvent, <number>event, handler)
@@ -70,7 +112,7 @@ namespace irBlocks
     //% weight=90
     //% blockId=IRKey
     //% block="IR key%key|was pressed"
-    export function irKey(key: irKeys): boolean
+    export function irKey(key: IRBirKeys): boolean
     {
         return (irCore.LastCode() == key)
     }
@@ -84,6 +126,17 @@ namespace irBlocks
     export function irCode(): number
     {
 	return irCore.LastCode()
+    }
+
+    /**
+      * IR Key Codes as number
+      */
+    //% weight=70
+    //% blockId=IRKeyCode
+    //% block="IR Key%key"
+    export function irKeyCode(key: IRBirNoAny): number
+    {
+	return key
     }
 
 }
