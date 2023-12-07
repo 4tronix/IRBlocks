3da2fb9b152227917c061e6e9591679b32b46a07
4tronix <Sales@4tronix.co.uk>
2023-12-07 16:32:40 +0000

    Add key code to number

--- irblocks.ts
+++ irblocks.ts
@@ -43,6 +43,47 @@ enum irKeys
 }
 
 /**
+  * IR Key code translations without the Any code
+  */
+enum irNoAny
+{
+    //% block="0"
+    Zero=152,
+    //% block="1"
+    One=162,
+    //% block="2"
+    Two=98,
+    //% block="3"
+    Three=226,
+    //% block="4"
+    Four=34,
+    //% block="5"
+    Five=2,
+    //% block="6"
+    Six=194,
+    //% block="7"
+    Seven=224,
+    //% block="8"
+    Eight=168,
+    //% block="9"
+    Nine=144,
+    //% block="star"
+    Star=104,
+    //% block="hash"
+    Hash=176,
+    //% block="up"
+    Up=24,
+    //% block="down"
+    Down=74,
+    //% block="left"
+    Left=16,
+    //% block="right"
+    Right=90,
+    //% block="ok"
+    Ok=56
+}
+
+/**
  * Makecode support for 4tronix IR Receiver
  */
 //% weight=50 color=#e7660b icon="\uf0a3"
@@ -57,7 +98,7 @@ namespace irBlocks
       */
     //% weight=100
     //% blockId=onIrEvent
-    //% block="on 14 IR pin%pin|key%key"
+    //% block="on IR pin%pin|key%key"
     export function onIREvent(pin: DigitalPin, event: irKeys, handler: Action)
     {
         irCore.initEvents(pin)
@@ -76,7 +117,7 @@ namespace irBlocks
     }
 
     /**
-      * Last IR Code received
+      * Last IR Code received as number
       */
     //% weight=80
     //% blockId=IRCode
@@ -84,6 +125,17 @@ namespace irBlocks
     export function irCode(): number
     {
 	return irCore.LastCode()
+    }
+
+    /**
+      * IR Key Codes as number
+      */
+    //% weight=70
+    //% blockId=IRKeyCode
+    //% block="IR Key%key"
+    export function irKeyCode(key: irNoAny): number
+    {
+	return key
     }
 
 }
